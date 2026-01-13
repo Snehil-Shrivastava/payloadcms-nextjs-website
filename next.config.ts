1a8db8e001373bd0f30883dd5d7ee0030ba0826e
Snehil-Shrivastava <snehil.shrivastava@xvscreations.com>
2026-01-13 10:24:26 +0530

    adding payload

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
+import { withPayload } from "@payloadcms/next/withPayload";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactCompiler: false,
 
   images: {
     dangerouslyAllowLocalIP: true,
@@ -16,4 +18,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPayload(nextConfig);
